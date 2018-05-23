7475259a60a49819457d4cd7af713d094a85dc02
Kevin Krummnacker <kk@dogado.de>
2018-05-24 00:33:48 +0200

    new config stub for api credentials

--- config/whmcs.php
+++ config/whmcs.php
@@ -1,10 +1,50 @@
 <?php
 
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | HTTP Driver
+    |--------------------------------------------------------------------------
+    |
+    | Supported: "guzzlehttp"
+    */
+
     'driver' => 'guzzlehttp',
-    'apiurl' => 'https://urltowhmcs.tld/whmcs/includes/api.php',
-    'username' => 'user',
-    'password' => 'pw123', // password without md5
+
+    /*
+    |--------------------------------------------------------------------------
+    | API Credentials
+    |--------------------------------------------------------------------------
+    |
+    | Enter the unhashed variant of your password if you use 'password' as 'auth_type'
+    |
+    | Supported auth types': "api", "password"
+    |
+    */
+
     'auth_type' => 'password',
+
+    'apiurl' => 'https://url.to.whmcs.tld/whmcs/includes/api.php',
+
+    'api' => [
+        'identifier' => 'YOUR_API_IDENTIFIER',
+        'secret' => 'YOUR_API_SECRET'
+    ],
+
+    'password' => [
+        'username' => 'YOUR_USERNAME',
+        'password' => 'YOUR_PASSWORD',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | ResponseType
+    |--------------------------------------------------------------------------
+    |
+    | Supported auth types': "json", "xml"
+    |
+    */
+
     'responsetype' => 'json'
 ];
