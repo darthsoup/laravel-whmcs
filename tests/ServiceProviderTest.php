11f15f24a3547f73f3841956ade8535a37349964
Stephan de Souza <blad3d@gmail.com>
2020-10-21 12:59:44 -0300

    Improving tests

--- tests/ServiceProviderTest.php
+++ tests/ServiceProviderTest.php
@@ -9,12 +9,20 @@ class ServiceProviderTest extends TestCase
     /** @test */
     public function ThrowAnExceptionIfConfigNotExist()
     {
-        $this->app['config']->set('whmcs.username', '');
-        $this->app['config']->set('whmcs.password', '');
+        $this->app['config']->set('whmcs.password.username', '');
+        $this->app['config']->set('whmcs.password.password', '');
 
         $this->expectException(\InvalidArgumentException::class);
 
         // call random WHMCS API
         Whmcs::GetProducts();
+    }
+
+    /** @test */
+    public function CanGetWhmcsProduct()
+    {
+        // call random WHMCS API
+        $result = Whmcs::GetProducts();
+        $this->assertNotNull($result);
     }
 }
